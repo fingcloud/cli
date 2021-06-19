560d401fca6a3817e3352ceb7c631a7f333f9208
Reza Morsali <light.reza@gmail.com>
2021-06-19 14:22:07 +0430

    feat: update spinner

--- internal/spinner/spinner.go
+++ internal/spinner/spinner.go
@@ -1,27 +1,40 @@
 package spinner
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/briandowns/spinner"
 )
 
-var s *spinner.Spinner
-
-func Start(msg ...string) {
-	s = spinner.New(spinner.CharSets[14], 100*time.Millisecond, spinner.WithColor("green"))
-	if len(msg) > 0 {
-		s.Suffix = fmt.Sprintf(" %s", msg[0])
-	}
-
-	s.Start()
+type Spinner struct {
+	*spinner.Spinner
+	text string
 }
 
-func Stop(msg ...string) {
-	if len(msg) > 0 {
-		s.FinalMSG = msg[0] + ""
+func New() *Spinner {
+	return &Spinner{
+		Spinner: spinner.New(spinner.CharSets[14], 100*time.Millisecond, spinner.WithColor("green")),
 	}
+}
 
-	s.Stop()
+func (s *Spinner) Start() *Spinner {
+	s.Spinner.Suffix = " " + s.text
+	s.Spinner.Stop()
+	s.Spinner.Start()
+	s.text = ""
+
+	return s
+}
+
+func (s *Spinner) Text(msg string) *Spinner {
+	s.text = msg
+	return s
+}
+
+func (s *Spinner) Stop() *Spinner {
+	s.Spinner.FinalMSG = s.text
+	s.Spinner.Stop()
+	s.text = ""
+
+	return s
 }
