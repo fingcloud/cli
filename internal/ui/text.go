55b4707803e2a18b3c0d493b56c7f0e9e9409024
Reza Morsali <light.reza@gmail.com>
2021-06-19 14:20:01 +0430

    add ui helpers

--- internal/ui/text.go
+++ internal/ui/text.go
@@ -6,26 +6,27 @@ var (
 	color = aurora.NewAurora(true)
 )
 
-func Bold(v interface{}) aurora.Value  { return color.Bold(v) }
-func Green(v interface{}) aurora.Value { return color.Green(v) }
-func Red(v interface{}) aurora.Value   { return color.Red(v) }
-func Blue(v interface{}) aurora.Value  { return color.BrightBlue(v) }
-func Gray(v interface{}) aurora.Value  { return color.Gray(14, v) }
+func Bold(v interface{}) aurora.Value   { return color.Bold(v) }
+func Green(v interface{}) aurora.Value  { return color.Green(v) }
+func Yellow(v interface{}) aurora.Value { return color.Yellow(v) }
+func Red(v interface{}) aurora.Value    { return color.Red(v) }
+func Blue(v interface{}) aurora.Value   { return color.BrightBlue(v) }
+func Gray(v interface{}) aurora.Value   { return color.Gray(14, v) }
 
 func Heading(v string) string {
 	return color.Sprintf(color.Bold("==> %s\n").Magenta(), Bold(v))
 }
 
 func Alert(v string) string {
-	return color.Sprintf(color.Bold("🚨 %s\n").Red(), v)
+	return color.Sprintf("%s %s", color.Red("==>"), color.Bold(v))
 }
 
-func Warning(text string) string {
-	return color.Sprintf(color.Bold("⚠️ %s\n").Yellow(), text)
+func Warning(v string) string {
+	return color.Sprintf("%s %s", color.Yellow("==>"), color.Bold(v))
 }
 
-func Info(text string) string {
-	return color.Sprintf(Gray(Bold("💁 %s\n").String()), text)
+func Info(v string) string {
+	return color.Sprintf("%s %s", color.Blue("==>"), color.Bold(v))
 }
 
 func KeyValue(key string, value interface{}) string {
