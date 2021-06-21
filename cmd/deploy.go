ca8bf5623110b4c2fca9e64e2fd389e7d9d32870
Reza Morsali <light.reza@gmail.com>
2021-06-21 08:44:39 +0430

    feat: improvments to deploy cmd

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -9,13 +9,14 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
+	"time"
 
-	"github.com/dustin/go-humanize"
+	"github.com/avast/retry-go"
 	"github.com/fingcloud/cli/api"
 	"github.com/fingcloud/cli/internal/cli"
 	"github.com/fingcloud/cli/internal/helpers"
-	"github.com/fingcloud/cli/internal/retry"
 	"github.com/fingcloud/cli/internal/ui"
+	pb "github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -50,120 +51,126 @@ func runDeploy(cli *cli.FingCli) {
 	files, err := helpers.GetFiles(path)
 	checkError(err)
 
-	var d *api.Deployment
-	err = retry.Retry(func() error {
-		deployment, changes, err := cli.Client.DeployemntCreate(app, &api.CreateDeploymentOptions{
+	var deployment *api.Deployment
+	err = retry.Do(func() error {
+		d, changes, err := cli.Client.DeployemntCreate(app, &api.CreateDeploymentOptions{
 			Files:  files,
 			Config: cli.Config,
 		})
 		if err != nil {
-			return err
+			return retry.Unrecoverable(err)
 		}
-
-		d = deployment
 
 		if changes != nil {
 			err = deployUploadChanges(cli, path, app, changes)
 			if err != nil {
-				return err
+				return retry.Unrecoverable(err)
 			}
+			return errors.New("retry deployment")
 		}
 
+		deployment = d
 		return nil
-	}, 3)
+	},
+		retry.Attempts(3),
+	)
 	checkError(err)
 
-	err = readBuildLogs(cli, app, d.ID)
+	err = readBuildLogs(cli, app, deployment.ID)
 	checkError(err)
-
 }
 
 func deployUploadChanges(cli *cli.FingCli, projectPath, app string, files []*api.FileInfo) error {
-	fmt.Println(ui.Info("Getting files..."))
+	fmt.Println(ui.Info("Getting changed files..."))
 	tarBuf := new(bytes.Buffer)
 	err := helpers.Compress(projectPath, files, tarBuf)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(ui.Info(fmt.Sprintf("Upload size %s, (%d files)", humanize.Bytes(uint64(tarBuf.Len())), len(files))))
-	fmt.Println(ui.Info("Uploading..."))
+	bar := pb.NewOptions64(
+		int64(tarBuf.Len()),
+		pb.OptionClearOnFinish(),
+		pb.OptionSetDescription("Uploading"),
+		pb.OptionSetTheme(pb.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+	)
+	updateProgress := func(n int64, max int64) {
+		bar.ChangeMax64(max)
+		bar.Set64(n)
+	}
+	defer bar.Clear()
 
-	return cli.Client.AppsUploadFiles(app, tarBuf)
+	return cli.Client.AppsUploadFiles(app, tarBuf, updateProgress)
 }
 
 func readBuildLogs(cli *cli.FingCli, app string, deploymentId int64) error {
 	fmt.Println(ui.Info("Building..."))
 
-	interrupt := make(chan os.Signal)
-	built := make(chan bool)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	interruptCh := make(chan os.Signal)
+	builtCh := make(chan bool, 1)
+	ticker := time.NewTicker(100 * time.Millisecond)
+	signal.Notify(interruptCh, os.Interrupt, syscall.SIGTERM)
 
-	var canceled bool
-	go func() {
-		defer func() {
-			close(interrupt)
-			close(built)
-		}()
-
-		for {
-			select {
-			case <-interrupt:
-				err := cli.Client.DeploymentCancel(app, deploymentId)
-				if err != nil {
-					checkError(err)
-				}
-				fmt.Println("")
-				return
-			case <-built:
-				return
-			}
-		}
+	defer func() {
+		close(interruptCh)
+		close(builtCh)
+		ticker.Stop()
 	}()
 
 	var from int64
 	var starting bool
 	for {
 		select {
-		case <-interrupt:
-			err := cli.Client.DeploymentCancel(app, deploymentId)
-			if err != nil {
-				checkError(err)
-			}
-		}
-		buildLogs, err := cli.Client.DeploymentListBuildLogs(app, deploymentId, &api.ListLogsOptions{From: from})
-		if err != nil {
-			return err
-		}
-
-		for _, log := range buildLogs.Logs {
-			fmt.Println(ui.Gray(log.Message))
-			if strings.HasPrefix(log.Message, "Successfully tagged") {
-				fmt.Println(ui.Info("Build completed"))
-				built <- true
-			}
-		}
-
-		if buildLogs.Deployment.Status == api.DeploymentStatusFailed {
-			return errors.New("Build failed")
-		}
-
-		if buildLogs.Deployment.Status == api.DeploymentStatusCancel || canceled {
-			return errors.New("Build canceled")
-		}
-
-		if buildLogs.Deployment.Status == api.DeploymentStatusStarting && !starting {
-			fmt.Println(ui.Info("Starting..."))
-			starting = true
-		}
-
-		if buildLogs.Deployment.Status == api.DeploymentStatusRunning {
-			fmt.Println(ui.Info("Deployment created :)"))
+		case <-builtCh:
 			return nil
-		}
+		case <-interruptCh:
+			return cli.Client.DeploymentCancel(app, deploymentId)
+		case <-ticker.C:
+			buildLogs, err := cli.Client.DeploymentListBuildLogs(app, deploymentId, &api.ListLogsOptions{From: from})
+			if err != nil {
+				return err
+			}
 
-		if len(buildLogs.Logs) > 0 {
-			from = buildLogs.Logs[len(buildLogs.Logs)-1].ID
+			go func() {
+				for _, log := range buildLogs.Logs {
+					if log.Message == "" {
+						continue
+					}
+					fmt.Println(ui.Gray(log.Message))
+					if strings.HasPrefix(log.Message, "Successfully tagged") {
+						fmt.Println(ui.Info("Build completed"))
+						builtCh <- true
+					}
+				}
+			}()
+
+			if buildLogs.Deployment.Status == api.DeploymentStatusFailed {
+				return errors.New("Build failed")
+			}
+
+			if buildLogs.Deployment.Status == api.DeploymentStatusCancel {
+				return errors.New("Build canceled")
+			}
+
+			if buildLogs.Deployment.Status == api.DeploymentStatusStarting && !starting {
+				fmt.Println(ui.Info("Starting..."))
+				starting = true
+			}
+
+			if buildLogs.Deployment.Status == api.DeploymentStatusRunning {
+				fmt.Println(ui.Info("Deployment created :)"))
+				return nil
+			}
+
+			if len(buildLogs.Logs) > 0 {
+				from = buildLogs.Logs[len(buildLogs.Logs)-1].ID
+			}
 		}
 	}
 }
