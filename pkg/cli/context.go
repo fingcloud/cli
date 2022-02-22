d7133d90e960f6b3f35fdd4ecde3ffa27f3fed47
Reza <light.reza@gmail.com>
2022-02-22 15:32:45 +0330

    add --auth && --use-auth to cli

--- pkg/cli/context.go
+++ pkg/cli/context.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/fingcloud/cli/pkg/api"
 	"github.com/fingcloud/cli/pkg/config/session"
+	"github.com/fingcloud/cli/pkg/ui"
 	"github.com/spf13/pflag"
+	"github.com/thoas/go-funk"
 )
 
 var (
@@ -20,29 +22,25 @@ type Context struct {
 
 	Client *api.Client
 
-	APIServer   *string
-	AccessToken *string
-
-	Path *string
+	APIServer   string
+	AccessToken string
+	Auth        string
+	AskAuth     bool
+	Path        string
 }
 
 func NewContext(out io.Writer, err io.Writer) *Context {
 	return &Context{
-		Stdout:      out,
-		Stderr:      err,
-		APIServer:   stringptr(""),
-		AccessToken: stringptr(""),
-		Path:        stringptr(""),
+		Stdout: out,
+		Stderr: err,
 	}
 }
 
 func (c *Context) AddFlags(flags *pflag.FlagSet) {
-	if c.AccessToken != nil {
-		flags.StringVar(c.AccessToken, "access-token", *c.AccessToken, "access token for the API server authentication")
-	}
-	if c.APIServer != nil {
-		flags.StringVar(c.APIServer, "server", *c.APIServer, "the address of the API server")
-	}
+	flags.StringVar(&c.AccessToken, "access-token", c.AccessToken, "access token for the API server authentication")
+	flags.StringVar(&c.APIServer, "server", c.APIServer, "the address of the API server")
+	flags.StringVar(&c.Auth, "auth", c.Auth, "specify auth account")
+	flags.BoolVar(&c.AskAuth, "use-auth", c.AskAuth, "show logged in auth to use")
 }
 
 func stringptr(val string) *string {
@@ -52,18 +50,36 @@ func stringptr(val string) *string {
 func (c *Context) SetupClient() error {
 	var accessToken string
 
+	if c.AskAuth {
+		sessions, err := session.Read()
+		if err != nil {
+			return err
+		}
+
+		options := funk.Map(sessions, func(s *session.Session) string { return s.Email }).([]string)
+		if err := ui.PromptSelect("select account", options, &c.Auth); err != nil {
+			return err
+		}
+	}
+
+	if c.Auth != "" {
+		if err := session.UseSession(c.Auth); err != nil {
+			return err
+		}
+	}
+
 	sess, _ := session.CurrentSession()
 	if sess != nil {
 		accessToken = sess.Token
 	}
 
-	if *c.AccessToken != "" {
-		accessToken = *c.AccessToken
+	if c.AccessToken != "" {
+		accessToken = c.AccessToken
 	}
 
 	opts := make([]api.Option, 0)
-	if *c.APIServer != "" {
-		opts = append(opts, api.WithBaseURL(*c.APIServer))
+	if c.APIServer != "" {
+		opts = append(opts, api.WithBaseURL(c.APIServer))
 	}
 
 	c.Client = api.NewClient(accessToken, opts...)
